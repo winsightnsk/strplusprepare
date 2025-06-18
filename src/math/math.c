1ba401aea5cc5462fa73d8ea3cf9eb32effbd53d
Obseen Costington <WinSightNSK@yandex.ru>
2025-06-18 18:15:14 +0700

    1

---
+++ src/math/math.c
@@ -0,0 +1,13 @@
+#include "math.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int divide(int a, int b) {
+    return (b == 0)? 0 : a / b;
+}
---
+++ src/math/math.h
@@ -0,0 +1,8 @@
+#ifndef MATH_H
+#define MATH_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int divide(int a, int b);
+
+#endif
