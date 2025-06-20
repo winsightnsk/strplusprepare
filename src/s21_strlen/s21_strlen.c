aa960786e754053e11fe2274cb3540f925aec8c2
rosenase <rosenase@student.21-school.ru>
2025-06-20 17:29:48 +0700

    add commit

--- src/s21_strlen/s21_strlen.c
+++ src/s21_strlen/s21_strlen.c
@@ -2,8 +2,8 @@
 
 s21_size_t s21_strlen(const char *str) {
   s21_size_t length = 0;
-  while (str[length] != '\0') {
+  while (str[length] != '\0') { //идем по строке пока не встретим конец строки
     length++;
   }
   return length;
-}
+}                                                                                                                   
