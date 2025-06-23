ff9498a53a44904d86f865a96596743d073c3327
AndreeWanya <russianjanek@gmail.com>
2025-06-23 19:38:21 +0700

    strchr for testing

---
+++ src/s21_strchr/s21_strchr.c
@@ -0,0 +1,12 @@
+#include "s21_strchr.h"
+
+char *s21_strchr(const char *str, int c) {
+  char *result = s21_NULL;
+  for (s21_size_t i = 0; i < s21_strlen(str); i++) {
+    if (str[i] == c) {
+      result = (char *)str + i;
+      i = s21_strlen(str);
+    }
+  }
+  return result;
+}
---
+++ src/s21_strchr/s21_strchr.h
@@ -0,0 +1,8 @@
+#ifndef S21_STRCHR
+#define S21_STRCHR
+
+#include "../common/basetypes.h"
+
+char *s21_strchr(const char *, int);
+
+#endif
