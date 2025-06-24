aef36b84021b50e7250000c2bdefefc7a3d0f50e
AndreeWanya <russianjanek@gmail.com>
2025-06-24 19:03:57 +0700

    Решена проблема с типами данных

--- src/s21_memcpy/s21_memcpy.c
+++ src/s21_memcpy/s21_memcpy.c
@@ -1,7 +1,7 @@
 #include "s21_memcpy.h"
 
 void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
-  for (int i = 0; i < n; i++) {
+  for (s21_size_t i = 0; i < n; i++) {
     ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
   }
   return dest;
