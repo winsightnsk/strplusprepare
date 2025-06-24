0cdf8b50caec572f6bd9eb78e8e10b9ed3c3417c
AndreeWanya <russianjanek@gmail.com>
2025-06-24 19:35:22 +0700

    Решение проблем с приведением типов

--- src/s21_memset/s21_memset.c
+++ src/s21_memset/s21_memset.c
@@ -1,8 +1,8 @@
 #include "s21_memset.h"
 
 void *s21_memset(void *str, int c, s21_size_t n) {
-  for (int i = 0; i < n; i++) {
-    ((unsigned char *)str)[i] = (unsigned char *)c;
+  for (s21_size_t i = 0; i < n; i++) {
+    ((unsigned char *)str)[i] = (unsigned char)c;
   }
   return str;
 }
