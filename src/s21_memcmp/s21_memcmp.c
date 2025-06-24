6711741fb8b39c3fbefb46dab8e3f471fbb2c9a0
AndreeWanya <russianjanek@gmail.com>
2025-06-24 18:48:33 +0700

    Решена проблема с несовместимостью типов

--- src/s21_memcmp/s21_memcmp.c
+++ src/s21_memcmp/s21_memcmp.c
@@ -2,7 +2,7 @@
 
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   int result = 0;
-  for (int i = 0; i < n; i++) {
+  for (s21_size_t i = 0; i < n; i++) {
     if (((unsigned char *)str1)[i] > ((unsigned char *)str2)[i]) {
       result++;
       i = n;
