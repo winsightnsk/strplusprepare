53746411e8c488e8afa0aa5ec2c4c42b8264bbfd
AndreeWanya <russianjanek@gmail.com>
2025-06-24 20:46:43 +0700

    Добавил + 1 в условие завершгения цикла, вроде ошибка ушла

--- src/s21_strchr/s21_strchr.c
+++ src/s21_strchr/s21_strchr.c
@@ -2,8 +2,8 @@
 
 char *s21_strchr(const char *str, int c) {
   char *result = s21_NULL;
-  for (s21_size_t i = 0; i < s21_strlen(str); i++) {
-    if (str[i] == c) {
+  for (s21_size_t i = 0; i < s21_strlen(str) + 1; i++) {
+    if (str[i] == (char)c) {
       result = (char *)str + i;
       i = s21_strlen(str);
     }
