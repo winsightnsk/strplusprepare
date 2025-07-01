663c9335fe5ca9584667719cfb1d47a35cceb48c
AndreeWanya <russianjanek@gmail.com>
2025-07-01 17:53:47 +0700

    Версия, проходящая тесты, Таисии спасибо!

--- src/s21_strstr/s21_strstr.c
+++ src/s21_strstr/s21_strstr.c
@@ -2,20 +2,15 @@
 
 char *s21_strstr(const char *haystack, const char *needle) {
   char *result = s21_NULL;
-  int flag = 0;
-  if (s21_strlen(haystack) >= s21_strlen(needle)) {
+  if (s21_strlen(needle) == 0) {
     result = (char *)haystack;
-    flag = 1;
   }
-  for (s21_size_t i = 0; i <= s21_strlen(haystack) - s21_strlen(needle) && flag && haystack[i] != '\0'; i++) {
+  for (s21_size_t i = 0; haystack[i] != '\0' && !result; i++) {
     if (haystack[i] == needle[0]) {
       result = (char *)haystack + i;
-      for (s21_size_t j = 0; j < s21_strlen(needle) && !result; j++) {
+      for (s21_size_t j = 1; j < s21_strlen(needle) && result; j++) {
         if (haystack[i + j] != needle[j]) {
           result = s21_NULL;
-        }
-        if (j == s21_strlen(needle)) {
-          flag = 0;
         }
       }
     }
