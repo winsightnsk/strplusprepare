d5546ee931d42bac5b26f761e51b0dea098fbf3f
AndreeWanya <russianjanek@gmail.com>
2025-07-07 22:10:53 +0700

    Подправил свой код, еще сырой, но уже должно работать

--- src/s21_strerror/s21_strerror.c
+++ src/s21_strerror/s21_strerror.c
@@ -3,7 +3,7 @@
 char *s21_strerror(int errnum) {
   static char message[50];
   if (errnum > ERR_SIZE || errnum < 0) {
-    sprintf(message, "Unknows error %d", errnum);
+    sprintf(message, STR_ERROR " %d", errnum);
   } else {
     s21_strncpy(message, ERRORS[errnum], 50);
   }
