b94f56c30aa82048bbb5844c53b58451b456849d
rosenase <rosenase@student.21-school.ru>
2025-07-17 02:41:51 +0700

    исправлено название  ширины

--- src/s21_sprintf/struct.h
+++ src/s21_sprintf/struct.h
@@ -5,7 +5,7 @@ typedef struct {
   int minus;     // к левому краю
   int plus;      // явно указать знак
   int space;     // доп символ под знак или пробел
-  int wight;     // ширина
+  int width;     // ширина
   int accuracy;  // точность
   int lenght;    // длина
   int flag_to_size;  // если число отрицательное или есть + или space
